5569ee71a56fd8c2ec38afb8607ad052bdaa3ff3
ellaidhurai n <ellaidhurai.97@gmail.com>
2023-05-17 13:07:49 +0530

    check

--- app/serializers.py
+++ app/serializers.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
